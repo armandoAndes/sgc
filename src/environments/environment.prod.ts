283b2a1e28ff3b9c606dd43ba92081b5d0204782
armandoAndes <j.ardilab@uniandes.edu.co>
2021-05-23 18:15:13 -0500

    enviro elb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   http:{
-    getAll: 'http://ec2-18-190-158-125.us-east-2.compute.amazonaws.com:8081/user',
-    create: 'http://ec2-18-190-158-125.us-east-2.compute.amazonaws.com:8081/boletinSave',
-    consulta: 'http://ec2-18-190-158-125.us-east-2.compute.amazonaws.com:8081/getBol'
+    getAll: 'http://elbMati-1439468736.us-east-2.elb.amazonaws.com/user',
+    create: 'http://elbMati-1439468736.us-east-2.elb.amazonaws.com/boletinSave',
+    consulta: 'http://elbMati-1439468736.us-east-2.elb.amazonaws.com/getBol'
   }
 };
