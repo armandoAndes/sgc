a0c95a6287541614720abbe54938e1f15744fbf5
armandoAndes <j.ardilab@uniandes.edu.co>
2021-05-23 17:29:54 -0500

    update endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   http:{
-    getAll: 'http://ec2-18-191-34-31.us-east-2.compute.amazonaws.com:8081/user',
-    create: 'http://ec2-18-191-34-31.us-east-2.compute.amazonaws.com:8081/boletinSave',
-    consulta: 'http://ec2-18-191-34-31.us-east-2.compute.amazonaws.com:8081/getBol'
+    getAll: 'http://ec2-18-190-158-125.us-east-2.compute.amazonaws.com:8081/user',
+    create: 'http://ec2-18-190-158-125.us-east-2.compute.amazonaws.com:8081/boletinSave',
+    consulta: 'http://ec2-18-190-158-125.us-east-2.compute.amazonaws.com:8081/getBol'
   }
 };
